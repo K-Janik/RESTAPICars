28889ecf4c8b8795daa34464a8a9ed37c173b036
Karol Janik <karol.jnk@gmail.com>
2021-05-12 21:46:26 +0200

    changes for Angular

--- src/main/java/pl/springboot2/karoljanik/rest/controller/CarController.java
+++ src/main/java/pl/springboot2/karoljanik/rest/controller/CarController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RequestMapping("/cars")
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class CarController {
 
     private final CarService carService;
