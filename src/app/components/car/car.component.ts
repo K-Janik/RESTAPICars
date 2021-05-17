37e7b1eb038218a83f40d4d20cbce86fae99e481
Karol Janik <karol.jnk@gmail.com>
2021-05-17 19:01:02 +0200

    homework week 3

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CarClientService, RootObject} from '../services/car-client.service';
+import {HttpErrorResponse} from '@angular/common/http';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-car',
@@ -9,18 +11,87 @@ import {CarClientService, RootObject} from '../services/car-client.service';
 export class CarComponent implements OnInit {
 
   messageForUser: string;
-  rootObject: RootObject;
+  public rootObject: RootObject[];
+  public editCar: RootObject;
+  public deleteCar: RootObject;
 
   constructor(private carClientService: CarClientService) { }
 
-  ngOnInit(): void {
-    this.carClientService.getCar().subscribe(value => {
-      this.rootObject = value;
-    });
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    this.getCars();
   }
 
-  // tslint:disable-next-line:typedef
-  sayHello(value: string) {
-    this.messageForUser = 'Cześć ' + value;
+  public getCars(): void {
+    this.carClientService.getCar().subscribe(
+      (response: RootObject[]) => {
+        this.rootObject = response;
+        console.log(this.rootObject);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+
+  public onAddCar(addForm: NgForm): void {
+    document.getElementById('add-car-form').click();
+    this.carClientService.insertCar(addForm.value).subscribe(
+      (response: RootObject) => {
+        console.log(response);
+        this.getCars();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateCarById(id: number, car: RootObject): void {
+    this.carClientService.updateCarById(id, car).subscribe(
+      (response: RootObject) => {
+        console.log(response);
+        this.getCars();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteCarById(carId: number): void {
+    this.carClientService.deleteCarById(carId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getCars();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onOpenModal(rootObject: RootObject, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addCarModal');
+    }
+    if (mode === 'edit') {
+      this.editCar = rootObject;
+      button.setAttribute('data-target', '#updateCarModal');
+    }
+    if (mode === 'delete') {
+      this.deleteCar = rootObject;
+      button.setAttribute('data-target', '#deleteCarModal');
+    }
+    container.appendChild(button);
+    button.click();
   }
 }
--- src/app/components/services/car-client.service.ts
+++ src/app/components/services/car-client.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {root} from 'rxjs/internal-compatibility';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarClientService {
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private httpClient: HttpClient) {
   }
 
-  public getCar(): Observable<RootObject> {
-    return this.httpClient.get<RootObject>('http://localhost:8080/cars');
+  public getCar(): Observable<RootObject[]> {
+    return this.httpClient.get<RootObject[]>(`${this.apiServerUrl}/cars`);
+  }
+  public insertCar(rootObject: RootObject): Observable<RootObject> {
+    return this.httpClient.post<RootObject>(`${this.apiServerUrl}/cars`, rootObject);
+  }
+  public updateCarById(carId: number, rootObject: RootObject): Observable<RootObject> {
+    return this.httpClient.put<RootObject>(`${this.apiServerUrl}/cars/${carId}`, rootObject);
+  }
+  public deleteCarById(carId: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiServerUrl}/cars/${carId}`);
   }
 }
 
